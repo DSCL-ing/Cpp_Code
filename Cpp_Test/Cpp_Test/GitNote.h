fb05644bca5e6d63a876f2ebd6947881e88e418e
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-01 20:00:41 +0800

    update

---
+++ Cpp_Test/Cpp_Test/GitNote.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+//Git：版本控制器
+/**
+ * 版本控制器是一个可以记录工程的每一次改动和版本迭代的一个管理系统，同时也方便多人协同作业。
+ * 
+ * Git 可以控制电脑上所有格式的文件，例如 doc、excel、dwg、dgn、rvt等等。
+ * 
+ * 文本文件可以知道修改的具体内容,非文本文件如二进制文件只能知道大概内容.如文件大小变化等(100k->200k)
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+//git centos 安装
+/**
+ * 查看版本 git --version
+ * 安装命令 sudo yum install git -y
+ * 卸载命令 sudo yum remove git -y
+ */
+
+
+//git管理:git仓库
+/**
+ * mkdir gitcode
+ * cd gitcode
+ * 初始化git仓库 git init
+ * 
+ */
+
+
+
+
+
