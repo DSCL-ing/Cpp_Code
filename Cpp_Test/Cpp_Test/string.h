6b89b0686e22d779c1aff660a3e5f4ed7cadd708
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-12 23:48:36 +0800

    update

--- Cpp_Test/Cpp_Test/string.h
+++ Cpp_Test/Cpp_Test/string.h
@@ -21,16 +21,32 @@ namespace test{
 		string()
 			//无参不能为空,因为一旦访问就会解引用空指针,不符合需求
 			//而且不能赋值为0 ,'\0', -- 
-			:_str(new char[1])
+			:_str(new char[1]) //必须使用new[]
 			, _size(0)
 			, _capacity(0)
 		{
 			_str[0] = '\0';
 		}
 
-		string(const string& s)
+		string(const char* s)
+			:_size(strlen(s))
+		{
+			_capacity = _size;
+			strcpy(_str, s);
+		}
+
+		//destructor
+		~string()
+		{
+			delete[] _str;
+			_size = _capacity = 0;
+		}
+
+
+		//copy constructor
+		string(const string& s) //拷贝构造的参数是本对象类型的引用
 			:_size(s._size)
-			,_capacity(s._capacity)
+			, _capacity(s._capacity)
 		{
 			//如果多个参数有关联,尽量不走初始化列表,防止因为声明位置导致初始化错误
 			//strlen计算不包含'\0'
@@ -41,22 +57,6 @@ namespace test{
 			strcpy(_str, s._str);
 		}
 
-		//destructor
-		~string()
-		{
-			delete[] _str;
-			_size = _capacity = 0;
-		}
-
-		//copy constructor
-		string(const char*& s)
-			:_size(strlen(s))
-		{
-			_capacity = _size;
-			_str = new char[_capacity + 1];
-			
-		}
-
 		//Capacity
 		size_t size()
 		{
@@ -64,7 +64,10 @@ namespace test{
 		}
 
 		//Element access
-		operator[]()
+		char& operator[](size_t pos)//必须返回真实数据地址
+		{
+			return _str[pos];
+		}
 
 		//String operator
 		const char* c_str()
@@ -78,7 +81,7 @@ namespace test{
 	{
 		string s1;
 		string s2("hello");
-		s2[0] = x;
+		s2[0] = 1;
 		cout << s1.c_str() << endl;
 		cout << s2.c_str() << endl;
 	}
