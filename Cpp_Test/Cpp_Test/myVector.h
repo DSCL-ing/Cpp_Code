8ea43eac90e0eabe936b9fc13079453d40dfb448
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-18 21:47:51 +0800

    update

--- Cpp_Test/Cpp_Test/myVector.h
+++ Cpp_Test/Cpp_Test/myVector.h
@@ -1,4 +1,62 @@
-﻿#pragma once
+﻿
+//note
+//参照string
+
+//C++二维vector (和二维数组不一样,与数组类似,用法超越了二维数组)
+/**
+ * 二维vector逻辑结构
+ * ----------行-------------
+ * 列列列列列列列列列列列列
+ *
+ * vector<vector<int>> vv ;
+ * vector<int>() //匿名对象,用于二维vector初始化
+ * 访问二维vector,vv[]是范围二维vector的元素,vv[][]是访问二维vector的元素的元素
+ * resize(i,vector<int>()) //开辟i个,类型为vector<int>的空间 //一般用于开辟二维vector的行和列
+ *
+ * //vector训练
+ * https://leetcode.cn/problems/pascals-triangle/submissions/
+ *
+ */
+
+ //iterator
+ /**
+  * 迭代器失效问题 - 原数据修改后需要重新初始化迭代器
+  *
+  * .
+  */
+
+  //capacity
+  /**
+   * resize()
+   * vector的resize使用得更频繁, 支持开空间加初始化
+   * .
+   */
+
+   //Modifiers
+   /**
+	* vector也不推荐使用insert和erase
+	* .
+	*/
+
+	//operator
+	/**
+	 * vector不提供find,推荐使用std::find(),string中有find是因为string的需求
+	 * 使用#include<algorithm>中的find
+	template<class InputIterator, class T>
+	  InputIterator find (InputIterator first, InputIterator last, const T& val)
+	{
+	  while (first!=last) {
+		if (*first==val) return first;
+		++first;
+	  }
+	  return last;
+	}
+	 *
+	 *
+	 *
+	 */
+
+#pragma once
 #include<assert.h>
 //模板参数省略:1.作为时  2.作为类型名
 template <typename T> //数组名:类型名:xx数组
