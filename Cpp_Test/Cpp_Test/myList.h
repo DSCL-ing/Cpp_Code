e65402892df08e67116447c21c8ef3d75857e9df
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-20 13:13:04 +0800

    Update myList.h

--- Cpp_Test/Cpp_Test/myList.h
+++ Cpp_Test/Cpp_Test/myList.h
@@ -7,45 +7,56 @@
  * begin是第一个元素的位置
  * end是最后一个元素的下一个位置,begin的前一个位置
  * 
- * 
+ * 成员类型重命名现在先不带指针(stl带指针和不带指针),统一规范一下,可读性好点,容易理解
  */
 
 //clear不清头节点
+
+//list的使用
+/**
+ * 1.需要std展开
+ * 2.定义list时需要带模板参数
+ * 
+ * 
+ * .
+ */
 
 #pragma once
 
 
 namespace test
 {
-		//struct默认权限是public,一般也不会加权限,class才会(需要封装时使用class)
+	//struct默认权限是public,一般也不会加权限,class才会(需要封装时使用class)
+	//结点使用struct的好处是开放结点,可以直接访问.不用重载流插入(stl也没重载),
+	
 	template<class T>
 	struct list_node
 	{
 		list_node<T>* _next;
 		list_node<T>* _prev;
-		T _data;
+		T _data;//
 
 		//使用全缺省匿名对象(只要是模板,都得传对象,自定义类型接收的必须是实体对象)构造
 		//list中使用是分配器分配空间,所以没写构造函数
-		list_node(const T& x = T())//此处为方便,直接重写构造
+		list_node(const T& x = T())//此处为方便不使用分配器,直接重写构造
 			: _next(nullptr)
 			, _prev(nullptr)
-			,  data(x)
+			, _data(x)
 		{}
 
 
 	};
 
-	//这里声明的是list的迭代器,自己的
+	//这里声明的是list专用的迭代器 -- 实例化后能用于访问list
 	//迭代器用于访问结点
 	template<class T>
 	struct __list_iterator
 	{
 		//成员类型
-		typedef __list_iterator<T>* iterator;
+		typedef __list_iterator<T> self;
 		typedef list_node<T> node;
 		//成员变量
-		node* _node;
+		node* _node; //成员变量只需要1个结点指针,通过这个结点指针的各种操作来模拟指针
 
 		//通过结点的指针来构造迭代器,模仿指针
 		iterator(node* n)
@@ -53,38 +64,48 @@ namespace test
 		{}
 
 		//指针操作运算符重载,模拟指针
-		operator++()
+
+		//解引用重载能够模拟指针返回的是结点的data,和string,vector迭代器一样
+		T& operator*()
 		{
-			_node
+			return _node->_data;
+		}
+		//箭头重载很难理解,编译器做了一定优化,后续再学习
+		T* operator->()
+		{
+			return &(_node->date);
+		}
+
+		self& operator++()
+		{
+			_node = _node->next;
 			return *this;
 		}
-		operator++(int)
+		//后置++返回的是一个拷贝,对原数据没有影响,安全
+		self operator++(int)
 		{
-
+			self tmp = *this;//将原数据拷贝一份
+			_node = _node->_next;
+			return tmp;//返回拷贝
 		}
-		operator--()
+		self& operator--()
 		{
-
+			_node = (*_node)._next;
+			return *this;
 		}
-		opratot--(int)
+		self opratot--(int)
 		{
-
+			self tmp = *this;
+			_node = _node->_next;
+			return tmp;
 		}
-		operator!=()
+		bool operator!=(const self& x)
 		{
-
+			return _node != x._node;
 		}
-		operator==()
+		bool  operator==(const self& x)
 		{
-
-		}
-		operator*()
-		{
-
-		}
-		operator->()
-		{
-
+			return _node == x._node;
 		}
 	};
 
@@ -94,6 +115,7 @@ namespace test
 	public:
 		typedef list_node<T> node;
 		typedef __list_iterator<T> iterator;
+		typedef const __list_iterator<T> const_iterator;
 	private:
 		node* _head;
 	public:
@@ -129,8 +151,63 @@ namespace test
 			_head->_prev = _head;
 		}
 
+		//iterator
+		//返回迭代器,目的是按规范通过迭代器访问结点
+		//虽然迭代器不是原生指针,但是list迭代器重载了解引用运算符*,可以和指针一样解引用就能得到数据
+		iterator begin()
+		{
+			//不能直接返回链表头结点的下一个的指针,因为链表的存储空间不连续,不能对指针直接加加减减,所以需要迭代器
+			
+			//需要实例化迭代器后才能使用迭代
+			//iterator it(_head->_next);
+			//return it;
+			
+			//可以使用C++提供的匿名对象来简化代码
+			return iterator(_head->_next);
+		}
+		const_iterator begin() const
+		{
+			return iterator(_head->_next)
+		}
+		iterator end()
+		{
+			//返回链表头结点的迭代器
+			return iterator(_head->_next);
+		}
+		const_iterator end()const
+		{
+			return iterator(_head->_next);
+		}
+
+
 		//list操作这边和数据结构的带头双向循环链表差不多,重点是iterator.写操作前写将迭代器整出来
 
+		//Capacity
+		//size实现需要迭代器计算链表长度会用就行
+
+
+		//modifiers
+		//增加结点
+
+		//删除结点//
+
+		//头删
+
+		//头插
+
+		//尾删
+
+		//尾插
+
+		//clear
+		void clear()
+		{
+
+		}
+
+		//element access
+		//返沪头结点
+		//返回尾结点
 
 	};
 
