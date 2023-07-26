3580b747b6fd3e19925564fe73844d66d493bb1e
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-26 11:13:24 +0800

    Update 20inheritance.h

--- Cpp_Test/Cpp_Test/20inheritance.h
+++ Cpp_Test/Cpp_Test/20inheritance.h
@@ -1,5 +1,21 @@
 ﻿#pragma once
 
+/**
+ * 特点:
+ * 1.继承的权限可以被缩小，但是不能放大，和const一样
+ *   继承下来的成员的的权限不会超过继承方式的权限
+ * 
+ * 2.保护和私有都能防外人
+ *   私有不仅能防外人还能防儿子
+ * 
+ * 3.子类可以通过父类的函数去调用父类的私有成员
+ * 
+ * 4.继承方式也可以不写
+ * a.对于class,默认继承方式是私有
+ * b.对于struct默认继承方式是公有
+ *  
+ */
+
 #include<string>
 #include<iostream>
 using std::cout;
@@ -18,8 +34,8 @@ public:
 		cout << "age:" << _age << endl;
 	}
 };
-
-class Student :public Person
+//   派生类derived  继承方式    基类 base_class
+class Student :     public     Person
 {
 protected:
 	int _stuid;
