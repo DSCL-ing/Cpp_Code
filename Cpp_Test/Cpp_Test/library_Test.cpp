6e101b05274bed3609125f18b08478cbd3fa0c2a
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-07-27 22:07:06 +0800

    update

--- Cpp_Test/Cpp_Test/library_Test.cpp
+++ Cpp_Test/Cpp_Test/library_Test.cpp
@@ -8,9 +8,22 @@ using std::endl;
 using std::cin;
 using std::swap;
 using std::max;
-int main12()
-{
 
+void fun(int a= 0, char b = 1)
+{
+	cout << "fun(int a, char b)" << endl;
+}
+
+int fun(int a = 1, char b =1)
+{
+	cout <<"fun(char b, int a)" << endl;
+	return 0;
+}
+
+int main()
+{
+	fun(1, 'a');
+	fun('a', 1);
 	return 0;
 }
 
