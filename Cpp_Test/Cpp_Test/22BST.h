1b2a9fa5498c7aed5e70860f2ddb779a63b48c55
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-08-02 21:42:44 +0800

    Update 22BST.h

--- Cpp_Test/Cpp_Test/22BST.h
+++ Cpp_Test/Cpp_Test/22BST.h
@@ -3,29 +3,31 @@
 //BST binary search tree /binary sort tree
 /**
  * 中文名:二叉搜索树或二叉排序树
- * 
- * 
- * 
+ *
+ *
+ *
  * 注:空树也是BST
  * 性质:
  * 左子树的所有键值小于根,根小于右子树的所有键值
- * 
+ *
  * 特点
  * 1.BST的中序/中根遍历的结果是升序排序
- * 
+ *
  * 2.默认的BST不允许冗余 -- 即不允许有重复的值
- * 
+ *
  * 3.插入的第一个值就是根
- * 
+ *
  * 4.删除
  * a.叶子结点:直接删
  * b.单路:续接
  * c.多路:尾删除法:用左子树的最大结点或右子树的最小结点
+ * $ 叶子结点可以并到单路去,总共有4种独立情况
+ * $ 多路有两种情况
  * 
  * 5.树的最大最小结点
  * a.一棵树中最大结点是最右结点(是右结点,且是最右边)
  * b.一棵树中最小结点是最左结点(是左结点,且是最左边)
- * 
+ *
  * 拓展:平衡二叉树,红黑树,AVL树
  */
 
@@ -59,7 +61,7 @@ namespace test
 
 		~BSTree()
 		{
-			
+
 		}
 
 		bool insert(const K& key)
@@ -77,7 +79,7 @@ namespace test
 			 *
 			 * 结束条件:cur为空 插入
 			 * //循环条件:cur不为空 继续走
-			 * 
+			 *
 			 */
 
 			if (_root == nullptr)
@@ -150,11 +152,11 @@ namespace test
 			 * 3.双路 尾删除法?找左子树的最大结点或右子树的最小结点
 			 *  结束条件:cur为空或cur._key == key
 			 * 循环条件:cur不为空
-			 * 
+			 *
 			 * 叶子和单路可以合并
-			 * 
+			 *
 			 * 删除后不需要置空,因为删除最后都会去链接下一个结点,而叶子结点下面都为空,最终一定会为空
-			 * 
+			 *
 			 */
 			node* parent = _root;
 			node* cur = _root;
@@ -174,106 +176,111 @@ namespace test
 				else
 				{
 
-					/**     
+					/**
 					 * 单路模拟图
-					 *          0				       0				 
-					 *        /    \			     /    \				 
-					 *       1      1			    1      1			 
-					 *      / \    / \			   / \    / \			 
-					 *     n   0  0   n			  0   n  n   0 			 
-					 * 
+					 *          0				       0
+					 *        /    \			     /    \
+					 *       1      1			    1      1
+					 *      / \    / \			   / \    / \
+					 *     n   0  0   n			  0   n  n   0
+					 *
 					 * 1为要删除的结点 ,0为结点 ,n为null
-					 * 
+					 *
 					 * 	1.左子树为空           2.右子树为空
-					 *            0	  0			        0  0		
-					 * 	         /     \		       /     \	
-					 * 	        1       1		      1       1	
-					 * 	       / \     / \		     / \     / \	
-					 * 	      n   0   n   0 	    0   n   0   n 
-					 * 
-					 * //只需要将删除结点的非空子树接到父亲上即可 -- 
-					 * 
+					 *            0	  0			        0  0
+					 * 	         /     \		       /     \
+					 * 	        1       1		      1       1
+					 * 	       / \     / \		     / \     / \
+					 * 	      n   0   n   0 	    0   n   0   n
+					 *
+					 * //只需要将删除结点的非空子树接到父亲上即可 --
+					 *
 					 */
 
-					//必须先从删除结点的左子树或右子树为空开始,然后再反向向上判断父亲
-					
-					//要删除的结点的左子树为空
+					 //必须先从删除结点的左子树或右子树为空开始,然后再反向向上判断父亲
+
+					 //要删除的结点的左子树为空
 					if (cur->_left == nullptr)
 					{
-						//处理根是要删除的结点前，必须先判断是单路或多路，所以if写在里面,写在外面会把多路吃掉,导致问题
+						//要删除的结点是根结点
 						if (cur == _root)
 						{
-							if (cur == _root)
+							//结点的左子树为空,就让根节点的右为新根
+							_root = cur->_right;
+						}
+						else//互斥:要删除的结点不是根结点
+						{
+							//删除的结点是父亲的左孩子
+							if (cur == parent->_left)
 							{
-								_root = cur->_left;
+								//由于要删除的结点的左子树为空,把右子树接上去成为父亲的新左孩子
+								parent->_left = cur->_right;
+							}
+							else //删除的结点是父亲的右孩子
+							{
+								//由于左子树为空,把右子树接上去成为父亲的新右孩子
+								parent->_right = cur->_right;
 							}
 						}
-						//删除的结点是父亲的左孩子吗
-						if (cur == parent->_left)
-						{
-							//左子树为空就把右子树接上去
-							parent->_left = cur->_right;
-						}
-						else //右孩子
-						{
-							parent->_right = cur->_right;
-						}
 						delete cur;
-						//cur = nullptr;
 					}
-					else if(cur->_right == nullptr)//要删除的结点的右子树为空
+					else if (cur->_right == nullptr)//要删除的结点的右子树为空
 					{
+						//删除的结点是根
 						if (cur == _root)
 						{
-							if (cur == _root)
+							//根节点的右为根,就让根节点的左为新根
+							_root = cur->_left;
+						}
+						else//互斥:删除的结点不是根
+						{
+							//删除的结点是父亲的左孩子
+							if (cur == parent->_left)
 							{
-								_root = cur->_left;
+								//要删除的结点的右子树为空,就把要删除的结点的左子树接上成为父亲的新左孩子
+								parent->_left = cur->_left;
 							}
+							else //互斥:删除的结点不是左孩子,是父亲的右孩子
+							{
+								//要删除的结点的右子树为空,就把要删除的结点的左子树接上成为父亲的新右孩子
+								parent->_right = cur->_left;
+							}
+							delete cur;
+							//cur = nullptr;
 						}
-						//删除的结点是父亲的左孩子吗
-						if (cur == parent->_left)
-						{
-							//右子树为空就把左子树接上去
-							parent->_left = cur->_left;
-						}
-						else //右孩子
-						{
-							parent->_right = cur->_left;
-						}
-						delete cur;
-						//cur = nullptr;
 					}
 					else
 					{
 						/**
-						 * 用左子树的最大结点或右子树的最小结点替代法(默认右子树)
 						 * 
+						 * 用左子树的最大结点或右子树的最小结点替代法(默认右子树)
+						 *
 						 * mr:右子树最小结点,1要删除的结点
 						 * 多路情况下与头结点无关,因为让代替的结点的key直接赋值给要删除的结点,最终删除的实际是替代结点
 						 * 通过key赋值从而达到删除
-						 * 
-						 * 情况一
-						 *        0                      1      
+						 *
+						 * 情况一 正常情况:右子树的最小结点是左孩子
+						 *        0                      1
 						 *       / \                    / \
 						 *      0   1                  0   0
 						 *         / \                    / \
 						 *        0   0                  mr  0
-						 *           / \                      \ 
+						 *           / \                      \
 						 *          mr  0                      0
-						 * 
-						 * 情况二
-						 * 	     0				         1      	
-						 * 	    / \				        / \		
-						 * 	   0   1			  	   0   mr		
-						 * 	      / \			  	        \		
-						 * 	     0   mr			  	         0	    
-						 * 		 	  \       	  	        / \	
-						 * 		 	   0      
-						 * 
-						 * 
+						 *
+						 * 情况二  特殊情况:右子树的最小结点是右孩子,且是要删除的结点的右孩子
+						 * 	     0				         1
+						 * 	    / \				        / \
+						 * 	   0   1			  	   0   mr
+						 * 	      / \			  	        \
+						 * 	     0   mr			  	         0
+						 * 		 	  \       	  	        / \
+						 * 		 	   0
+						 *
+						 *
 						 */
-						node* pminRight = cur;
-						node* minRight = cur->_right;
+						node* pminRight = cur; //要删除的结点的右子树的最小结点的父亲
+						node* minRight = cur->_right;//要删除的结点的右子树的最小结点
 						//找右子树最小结点
 						while (minRight->_left)
 						{
@@ -286,31 +293,32 @@ namespace test
 						//替代完成后,需要回收minrRight结点 -- 回收处理
 						if (pminRight->_right == minRight)//如果循环没有进去,即第一个minRight->_left为空,right可能不为空
 						{
+							//右子树的最小结点成为父结点的新右子树
 							pminRight->_right = minRight->_right;
-
 						}
 						else //循环进入了
 						{
+							//右子树的最小结点成为父结点的新右子树
 							pminRight->_left = minRight->_right;
 						}
 
 						delete minRight;
-
 					}
+					return true;
 				}
 
-			}	
+			}
 			return false;
 		}
 
 		//无参递归常用方法：套壳：
 		/**
 		 * 由于递归需要访问到私有成员,而外界访问私有成员需要友元或使用接口get什么什么
-		 * 
+		 *
 		 * C++一般不喜欢使用get,所以引出了套壳
-		 * 
+		 *
 		 * 套壳:壳面向对象,壳内调用私有,面对对象隐藏细节
-		 * 
+		 *
 		 * 能不能用缺省参数root? 我的理解是,缺省参数在传在this之前,即先传缺省再传this,接收此时的缺省参数root没有this
 		 * //不行,因为缺省参数访问成员需要this,但是this不可以作为缺省参数,缺省参数需要是全局变量或是常量,
 		 */
@@ -320,7 +328,7 @@ namespace test
 		}
 
 	private:
-		
+
 		void _InOrderTraversal(node* root)
 		{
 			if (root == nullptr)
@@ -336,12 +344,12 @@ namespace test
 
 	void test_BST1()
 	{
-		
+
 		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
 		BSTree<int> bst;
 
 		//测试插入
-		for (auto i:a)
+		for (auto i : a)
 		{
 			bst.insert(i);
 		}
@@ -349,7 +357,7 @@ namespace test
 
 
 		//测试find
-		cout <<  endl;
+		cout << endl;
 		for (auto i : a)
 		{
 			if (bst.find(i))
@@ -363,22 +371,23 @@ namespace test
 		}
 
 		////删除单路和叶子测试
-		bst.erase(10);
-		bst.erase(14);
-		bst.erase(13);
-		bst.erase(1);
-		bst.erase(4);
-		bst.erase(6);
-		bst.erase(3);
-		bst.erase(8);  // 根没处理好前先别删
-		bst.erase(7);
-		bst.InOrderTraversal();
+		//bst.erase(10);
+		//bst.erase(14);
+		//bst.erase(13);
+		//bst.erase(1);
+		//bst.erase(4);
+		//bst.erase(6);
+		//bst.erase(3);
+		//bst.erase(8);  // 根没处理好前先别删
+		//bst.erase(7);
+		//bst.InOrderTraversal();
 
 		//删除多路测试
-		//bst.erase(8);
-		//bst.erase(3);
-		//bst.erase(14);
-		//bst.erase(14);
+		bst.erase(8);
+		bst.erase(10);
+		bst.erase(3);
+		bst.erase(14);
+		bst.erase(1);
 		bst.InOrderTraversal();
 
 	}
