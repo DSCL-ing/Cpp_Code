0aa22ce6482399028bf5aed576ac3f34a813a506
DSCL-ing <102305029+DSCL-ing@users.noreply.github.com>
2023-10-16 23:37:08 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
 
 //#include <ctime>
 //#include <random>
@@ -10,6 +10,13 @@ using std::endl;
 #include<string.h>
 int main()
 {
+	char str[3] = { 0 };
+	char buf[3] = { '\0' };
+
+	char ch = 0;
+	//char* c = '\0';
+	cout << sizeof(str[0]) << endl;
+	//cout << sizeof(c) << endl;
 	return 0;
 }
 
