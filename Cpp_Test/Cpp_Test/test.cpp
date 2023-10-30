ff723509fcc0b71d77af7336312b79bee806d329
DSCL-ing <2639075023@qq.com>
2023-10-30 23:01:06 +0800

    OS Core-page Core-status Schedule...

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -9,11 +9,26 @@ using std::cout;
 using std::endl;
 #include<string.h>
 #include<algorithm>
+
+struct TreeNode
+{
+	TreeNode* leff;
+	TreeNode* right;
+	int data;
+};
+
+int TreeSize(TreeNode* root )
+{
+	if (root != nullptr) return 1;
+	return TreeSize(root->leff) + TreeSize(root->right) + 1;
+}
+
 int main()
 {
-	int arr[10] = { 0 };
-	*(arr + 2);
-	printf("hello");
+	TreeNode* root;
+	TreeSize(root);
+
+
 	return 0;
 }
 
