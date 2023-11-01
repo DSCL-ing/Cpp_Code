14dfdd4a761549dad99c6112903958f1a17b72f9
DSCL-ing <2639075023@qq.com>
2023-11-01 14:05:30 +0800

    signal

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -12,15 +12,17 @@ using std::endl;
 
 struct TreeNode
 {
-	TreeNode* leff;
+	TreeNode* left;
 	TreeNode* right;
 	int data;
 };
 
 int TreeSize(TreeNode* root )
 {
-	if (root != nullptr) return 1;
-	return TreeSize(root->leff) + TreeSize(root->right) + 1;
+	if (root == nullptr) return 0;
+	if ((root->left == nullptr && root->right == nullptr)) return 1;
+	return (TreeSize(root->left) + TreeSize(root->right));
+	
 }
 
 int main()
