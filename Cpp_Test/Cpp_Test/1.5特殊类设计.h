4690a94fd9264ee9b1053a1b4d6c5ed74ad68a6a
DSCL-ing <2639075023@qq.com>
2023-12-08 18:05:17 +0800

    设计特殊类

---
+++ Cpp_Test/Cpp_Test/1.5特殊类设计.h
@@ -0,0 +1,94 @@
+﻿#pragma once
+
+
+// 1. 设计一个不能被拷贝的类
+/*
+ 解析:拷贝只会放生在两个场景中：拷贝构造函数以及赋值运算符重载，因此想要让一个类禁止拷贝，
+     只需让该类不能调用拷贝构造函数以及赋值运算符重载即可。
+
+C++98;
+    将拷贝构造函数与赋值运算符重载只声明不定义，并且将其访问权限设置为私有即可。
+原因：
+1. 设置成私有：如果只声明没有设置成private，用户自己如果在类外定义了，就可以不能禁止拷贝了
+2. 只声明不定义：防止编译器自动生成,且没必要定义,原本就是不想使用
+
+C++11:
+    C++11扩展delete的用法，delete除了释放new申请的资源外，如果在默认成员函数后跟上 =delete，表示让编译器删除掉该默认成员函数。
+class CopyBan //ban 禁止 , 禁止拷贝
+{
+    // ...
+    CopyBan(const CopyBan&)=delete;
+    CopyBan& operator=(const CopyBan&)=delete;
+    //...
+};
+
+h*/
+
+
+//2.设计一个只能在堆上创建对象的类
+//------------------------------------------------------------ 只能在堆上创建对象的类
+/*
+//思路1:隐藏析构函数
+class HeapOnly
+{
+public:
+
+    void Destory()
+    {
+        delete this;
+    }
+
+private:
+    ~HeapOnly()
+    {
+        std::cout << "~HeapOnly()" << std::endl;
+    }
+    //public:
+    int _x;
+};
+
+int main()
+{
+
+    //HeapOnly ho1; //报错,没有可访问的析构函数类不能在栈上定义对象,因为栈上的对象是由编译器负责析构的,但类外不能访问私有,即编译器调不到析构,释放不了,所以会有问题
+    //HeapOnly ho2(); //未知,编译器自动生成的行为不可控制
+
+    //static HeapOnly ho4; //静态区的对象也受编译器控制析构,所以也不能正常定义
+
+    HeapOnly *ho3 = new HeapOnly; //没有可访问的析构函数类只能在堆上定义对象,因为堆上的资源由用户自己释放,所以可以正常定义
+   //delete ho3; //必须是公开的析构函数,才能在栈上使用delete(在栈域访问类域私有成员)
+   ho3->Destory(); //进到类域内部的函数能够访问私有成员,可以析构
+    return 0;
+}
+*/
+
+//思路2:隐藏构造函数(常用,通用)
+//封构造函数能够把在栈上 定义或new 封掉
+/* class HeapOnly
+{
+public:
+    static HeapOnly* createObj(int x = 0) // static函数在类域内,不受访问修饰符约束,能够看得见成员函数
+    {
+        HeapOnly* p = new HeapOnly(x); //在类域内new一个对象,就是在类域内调用构造,定义的空间位于堆区
+        return p;
+    }
+private:
+    HeapOnly(int x = 0) :_x(0)
+    {}
+    HeapOnly(const HeapOnly& ho) = delete;
+    HeapOnly& operator=(const HeapOnly& ho) = delete;
+    int _x;
+};
+
+int main()
+{
+    HeapOnly* ho = HeapOnly::createObj(0); //在栈上定义一个指针,调用类域内的静态函数createObj去new一个对象,得到对象的指针
+        //HeapOnly ho1(*ho); //还需要禁掉拷贝构造
+    return 0;
+}
+*/
+
+//------------------------------------------------------------ 只能在堆上创建对象的类__End;
+
+
+
--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -12,147 +12,30 @@
 #include<thread>
 #include<functional>
 
-template<class T>
-class shared_ptr
+
+// 设计只能在栈上定义类(不能完全做到,最多只能禁止堆,不能禁止static区)
+class HeapOnly
 {
 public:
-    //template <class U> explicit shared_ptr(U* p);
-    explicit shared_ptr(T* ptr = nullptr) :_ptr(ptr), _pcount(new int(1)), _pmtx(new std::mutex()) //第一次定义
-    {}
-
-    //with deleter(4) .接收定制删除器版本
-    //template <class U, class D> shared_ptr(U* p, D del);
-    template <class T, class D>
-    shared_ptr(T* ptr, D del)
-        : _ptr(ptr), _pcount(new int(1)), _pmtx(new std::mutex()), _del(del)
-    {}
-
-    shared_ptr(const shared_ptr<T>& sp) :_ptr(sp._ptr), _pcount(sp._pcount), _pmtx(sp._pmtx) //拷贝
+    static HeapOnly createObj(int x = 0) 
     {
-        AddCount();
-    }
-
-    ~shared_ptr()
-    {
-        Release();
+        HeapOnly ho;
+        return ho;
     }
 private:
-    void Release()
-    {
-        _pmtx->lock();
-
-        bool deleteFlag = false;
-
-        if (--(*_pcount) == 0)//每次执行都会--, 如果--后为0,就析构
-        {
-            if (_ptr)
-            {
-                //delete _ptr;
-                _del(_ptr);//把_ptr传给 通过包装器接收的 来自外界或缺省的 定制删除器
-            }
-
-
-            delete _pcount;
-            //锁也要释放
-
-            //{ //解决方案一
-            //_pmtx->unlock();
-            //delelte _pmtx;
-            //return;
-            //}
-
-            //解决方案二 : 使用标志位
-            deleteFlag = true;
-
-        }
-        _pmtx->unlock(); //释放锁后,不能有解锁操作
-
-        if (deleteFlag)
-        {
-            delete _pmtx;
-        }
-
-    }
-    void AddCount()
-    {
-        _pmtx->lock();
-        ++(*_pcount);
-        _pmtx->unlock();
-    }
-public:
-    shared_ptr<T>& operator=(const shared_ptr<T>& sp)
-    {
-        //先析构掉自己的资源
-        if (sp._ptr != _ptr)
-        {
-            Release();
-            _ptr = sp._ptr;
-            _pcount = sp._pcount;
-            _pmtx = sp._pmtx;  //拷贝时也要把锁烤过来
-            AddCount();
-        }
-        return *this;
-    }
-    T& operator*()
-    {
-        return *_ptr;
-    }
-    T* operator->()
-    {
-        return _ptr; //返回地址
-    }
-    T* get() const //getPtr
-    {
-        return _ptr;
-    }
-    int use_count()  //返回引用的对象个数
-    {
-        return *_pcount;
-    }
-private:
-    T* _ptr;    //管理资源的指针
-    int* _pcount; //动态开辟的计数器,用于引用计数
-    std::mutex* _pmtx; //多线程时,保障临界资源的安全
-    std::function<void(T*)> _del = [](T*ptr) {       //D是可调用对象类型,具体类型有多种,functional类型能够一个定义全部接收
-        std::cout << "default function<void(T*)>" << std::endl;
-        delete ptr;                         //缺省值
-    };
+    HeapOnly(int x = 0):_x(0)
+    {}
+    //HeapOnly(const HeapOnly& ho) = delete;
+    //HeapOnly& operator=(const HeapOnly& ho) = delete;
+    int _x;
 };
-
-/*
-不同类型的资源有不同的析构方式, shared_ptr通过可调用对象:定制删除器 来实现不同方式的资源释放
-定制删除器:就是可调用对象,实现一个定制的,具有删除功能的可调用对象,传给shared_ptr
-*/
-template<class T>
-struct DeleteArray
-{
-    void operator()(T* ptr) //shared_ptr会把资源的指针传进来
-    {
-        std::cout << " void operator()(T* ptr)" << std::endl;
-        delete[] ptr;
-    }
-};
-
-struct Date
-{
-    int _year = 0;
-    int _month = 0;
-    int _day = 0;
-};
-
 
 int main()
 {
-    shared_ptr<Date> sp0{ new Date };
-    shared_ptr<Date> spa1{ new Date[4],DeleteArray<Date>() }; // 仿函数方式 释放资源
-    shared_ptr<Date> spa2{ new Date[10],[](Date* ptr) {delete[] ptr; } }; //lambda表达式方式 释放资源
-    shared_ptr<FILE> spF3{ fopen("test.cpp","r"),[](FILE* ptr) {fclose(ptr); } }; //不只是堆空间,还可以是文件
+    HeapOnly ho =  HeapOnly::createObj(0); //在栈上定义一个指针,调用类域内的静态函数createObj去new一个对象,得到对象的指针
+    //HeapOnly ho1(*ho); //还需要禁掉拷贝构造
     return 0;
 }
-
-
-
-
 
 
 
