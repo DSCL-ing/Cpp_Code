f17b28ede1ccaf6d0076d75aba8976ee86021016
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2023-12-19 22:04:12 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -14,16 +14,26 @@
 #include<vector>
 
 
-
-
+class A {
+public:
+    virtual void f() {}
+};
+class B : public A
+{};
+void fun(A* pa) {
+    B* pb1 = static_cast<B*>(pa); //C风格直接强转也可以
+    // 如果是pa指向的是A对象,则pb会变成子类类型的父类对象,因为pb是子类类型,能够访问到子类成员,如果此时pa访问子类成员,就会造成越界.因此不安全.
+    B* pb2 = dynamic_cast<B*>(pa);
+    std::cout << "pb1:" << pb1 << std::endl;
+    std::cout << "pb2:" << pb2 << std::endl;
+}
 int main()
 {
-    volatile const  int a = 2;
-    int* p = const_cast<int*>(&a);
-    *p = 3;
-    std::cout << a << std::endl;
-    std::cout << *p << std::endl;
-    return 0 ;
+    A a;
+    B b;
+    fun(&a); //不安全
+    fun(&b); //安全
+    return 0;
 }
 
 
