6b2e8f47915cadae1fcaced171a8752896dbdae1
chj <2639075023@qq.com>
2024-02-22 22:25:24 +0800

    CloudBackup_client

---
+++ CloudBackup_Client/CloudBackup_Client/client.cpp
@@ -0,0 +1,13 @@
+﻿#include"util.hpp"
+
+int main()
+{
+    ns_cloud_backup::FileUtil fu("./");
+    std::vector<std::string> arry;
+    fu.ScanDirectory(arry);
+    for (auto& i : arry)
+    {
+        std::cout<<i<<std::endl;
+    }
+    return 0;
+}
