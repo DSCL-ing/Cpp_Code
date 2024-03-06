55d91eda946d382954cd15440048ec3ecc156a46
chj <2639075023@qq.com>
2024-03-06 22:59:49 +0800

    IP

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,9 +1,26 @@
-﻿
-#include<iostream>
-
-int main()
+﻿#include <iostream>
+#include <QApplication>
+#include <QWidget>
+class Test
 {
-    int * const a = nullptr; 
-    int &ref =*a ;
-    return 0;
-}const *
+public:
+    Test(QWidget* ui = nullptr) {}
+    void show() { ui->show(); }
+    int count() const { return m_count; }
+private:
+    QWidget* ui;
+    int m_count = 0;
+};
+void showTest(const Test& t)
+{
+    std::cout << t.count() << std::endl;
+    t.show();
+}
+int main(int argc, char* argv[])
+{
+    //QApplication a(argc, argv);
+    void* w;
+    Test t(w);
+    showTest(t);
+    return a.exec();
+}
