30002bbb7f52a15e779b3dbc00e442ff3b4afd61
chj <2639075023@qq.com>
2024-03-20 00:15:48 +0800

    非阻塞IO select

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,6 +1,25 @@
 ﻿#include<iostream>
+class Base
+{
+public:
+    int x;
+};
+
+class Derive : public Base
+{
+    int y;
+};
+
+void fun(Base* b)
+{
+    b[1].x = 1;
+}
+
 int main()
 {
-    std::cout<<stdin<<std::endl;
+    Derive arr[5];
+    fun(arr);
+    std::cout<<arr[1].x<<std::endl;
 
+    return 0;
 }
