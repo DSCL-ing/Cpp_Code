798c2b6564177a3edf31721a6389b42405dfd094
chj <2639075023@qq.com>
2024-05-25 22:52:23 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -2,30 +2,41 @@
 #include<cstring>
 #include<iostream>
 #include<algorithm>
+#include"9Date.h"
 
 template<class T>
-void func(T* t)
-{
-    std::cout<<"Function template"<<std::endl;
-    std::cout<<t<<std::endl;
-}
+struct Less {
+    bool operator()(const T& left, const T& right) {
+        return left < right;
+    }
+};
 
-//template<class T>
-//void func<T*>(T* t)
-//{
-//    std::cout<<"Function Template Specialization "<<std::endl;
-//}
-
+////全特化
 //template<>
-//void func<int*>(int*p)
-//{
-//
-//    std::cout<<"Function"<<std::endl;
-//}
+//struct Less<Date*> {
+//    bool operator()(Date* left, Date* right) {
+//        std::cout<<"full specialization"<<std::endl;
+//        return *left < *right;
+//    }
+//};
+
+//偏特化
+template<class T>
+struct Less<T*> {
+    bool operator()(T* left, T* right) {
+        std::cout<<"partial specialization"<<std::endl;
+        return *left < *right;
+    }
+};
+
+
 
 int main()
 {
-    func<int>(nullptr);
-    //func(1);
+    Date d1(1111,11,11); 
+    Date d2(2222,12,12);
+        
+    Less<Date*>()(&d1,&d2);
+
     return 0;
 }
