eb282056f8febcab4d6523940ebcca51c2e5cd1e
chj <2639075023@qq.com>
2024-06-01 22:41:01 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -5,20 +5,21 @@
 
 class Person {
 public:
-    ~Person() { std::cout << "~Person()" << "\n"; }
+      ~Person() { std::cout << "~Person()" << "\n"; }
 };
 
 class Student :public Person {
 public:
-    ~Student() { std::cout << "~Student()" << "\n"; }
+      ~Student() { std::cout << "~Student()" << "\n"; }
 };
 
 int main()
 {
-    Person* ptr1 = new Student; 
+    Person* ptr1 = new Person; 
     Person* ptr2 = new Student;
 
     delete ptr1;
     delete ptr2;
+
     return 0;
 }
