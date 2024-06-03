f255696989dfab4887734201bad7f049ad548a1d
chj <2639075023@qq.com>
2024-06-03 17:40:24 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -4,17 +4,12 @@
 #include<algorithm>
 
 
-class A
+class A final
 {
-public:
-    virtual void func(int val = 1) { std::cout << "A->" << val << std::endl; }
-    virtual void test() { func(); }
 };
 
 class B : public A
 {
-public:
-    void func(int val = 0) { std::cout << "B->" << val << std::endl; }
 };
 
 int main(int argc, char* argv[])
