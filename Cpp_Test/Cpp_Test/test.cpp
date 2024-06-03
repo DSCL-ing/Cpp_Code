d8920d95634d92c8d1b641741bf9c7251beeb6a2
chj <2639075023@qq.com>
2024-06-03 12:47:27 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -3,33 +3,47 @@
 #include<iostream>
 #include<algorithm>
 
-class A{};
-class B : public A{};
 
-class Person {
+class A
+{
 public:
-    virtual A* BuyTicket() {
-        std::cout << "全票" << std::endl;
-        return nullptr;
-    }
+    virtual void func(int val = 1) { std::cout << "A->" << val << std::endl; }
+    virtual void test() { func(); }
 };
 
-class Student :public Person {
+class B : public A
+{
 public:
-    A* BuyTicket() {
-        std::cout << "半票" << std::endl;
-        return nullptr;
-
-    }
+    void func(int val = 0) { std::cout << "B->" << val << std::endl; }
 };
-void func(Person& p){
-    p.BuyTicket();
-}
 
-int main(){
-    Person p;    
-    Student s;    
-    func(p);     
-    func(s);      
+int main(int argc, char* argv[])
+{
+    B* p = new B;
+    p->test();
     return 0;
 }
+
+//class Person {
+//public:
+//     void BuyTicket(int val = 1) {
+//        std::cout << "全票" <<":"<<val<<"\n";
+//    }
+//    void call() {
+//        BuyTicket();
+//    }
+//};
+//
+//class Student :public Person {
+//public:
+//    void BuyTicket(int val = 0) {
+//        std::cout << "半票" <<"="<<val<<"\n";
+//    }
+//};
+//
+//int main(){
+//    Student s;
+//    s.call();
+//    s.BuyTicket();
+//    return 0;
+//}
