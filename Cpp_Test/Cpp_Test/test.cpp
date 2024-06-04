e07b644aa68d3f926d9ed4779bdb6b467b8276bc
chj <2639075023@qq.com>
2024-06-04 14:32:18 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -6,18 +6,91 @@
 
 class A {
 public:
-     virtual void func()  {}
+    A(int a):_a(a) {
+    }
+    int _a ;
 };
 
-class B : public A {
+class B :virtual public A {
 public:
-    void func(int i) override{ }
+    B(int b)    :A(2)
+    {
+    }
+    int _b = 2;
 };
 
-int main(int argc, char* argv[])
-{
+
+class C :virtual public A {
+public:
+    C(int c) : A(3){
+    }
+    int _c = 3;
+};
+
+class D :public C, public B {
+public:
+    D():B(4),C(5),A(6){}
+    int _d = 4;
+};
+
+int main() {
+    //D *p = new D;
+    D d;
+
+    int a[3] = {1,2,3};
+
     return 0;
 }
+
+//class A {
+////public:
+//private:
+//    int _a = 1;
+//};
+//
+//class B : public A {
+//public:
+//    int _b = 2;
+//};
+//
+//class C : public A {
+//public:
+//    int _c = 3;
+//};
+//
+//class D :public B, public C {
+//public:
+//    int _d = 4;
+//};
+//
+//int main()
+//{
+//    //B b;
+//    //C c;
+//    D d;
+//    std::cout<<sizeof(D)<<"\n";
+//}
+//
+
+//class Base{
+//public:
+//     void func() {}
+//private:
+//    int _a;
+//    char _b;
+//};
+//
+//
+//class Driver :public Base {
+//
+//};
+//
+//int main(int argc, char* argv[])
+//{
+//    std::cout<<sizeof(Base)<<"\n";
+//    std::cout<<sizeof(Driver)<<"\n";
+//    return 0;
+//}
 
 //class Person {
 //public:
