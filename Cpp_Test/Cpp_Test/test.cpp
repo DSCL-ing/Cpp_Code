336123e5aa6af386b6a8983ec65d9786156dda43
chj <2639075023@qq.com>
2024-06-14 22:50:59 +0800

    智能指针

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,7 +1,4 @@
-﻿#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
+﻿#include<iostream>
 
 template<class T>
 class SmartPtr {
@@ -12,35 +9,51 @@ public:
         delete _ptr;
         std::cout<<"delete ptr"<<"\n";
     }
+    T& operator*() {
+        return *_ptr;
+    }
+    T* operator->() {
+        return _ptr;
+    }
 private:
     T*_ptr;
 };
 
-int div()
+int main()
 {
-    int a, b;
-    std::cin >> a >> b;
-    if (b == 0)
-        throw std::invalid_argument("除0错误");
-    return a / b;
-}
-
-void example() {
     SmartPtr<int> sp1(new int);
     SmartPtr<int> sp2(new int);
-    try {
-        div();
-    }
-    catch (...) {
-        throw;
-    }
+    *sp1 = 10; 
+    std::cout<< *sp1 <<std::endl;
+
+    return 0;
 }
 
-int main() {
-    try {
-        example();
-    }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
-}
+//int div()
+//{
+//    int a, b;
+//    std::cin >> a >> b;
+//    if (b == 0)
+//        throw std::invalid_argument("除0错误");
+//    return a / b;
+//}
+//
+//void example() {
+//    SmartPtr<int> sp1(new int);
+//    SmartPtr<int> sp2(new int);
+//    try {
+//        div();
+//    }
+//    catch (...) {
+//        throw;
+//    }
+//}
+//
+//int main() {
+//    try {
+//        example();
+//    }
+//    catch (std::exception& e) {
+//        std::cout << e.what() << std::endl;
+//    }
+//}
