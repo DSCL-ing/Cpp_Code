89453977d5fad9ee9cc6b670bfda71b4fbefc2c7
chj <2639075023@qq.com>
2024-06-16 14:39:31 +0800

    智能指针

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,31 +1,73 @@
 ﻿#include<iostream>
 
-template<class T>
-class SmartPtr {
-public:
-    SmartPtr(T*ptr):_ptr(ptr) 
-    {}
-    ~SmartPtr() {
-        delete _ptr;
-        std::cout<<"delete ptr"<<"\n";
-    }
-    T& operator*() {
-        return *_ptr;
-    }
-    T* operator->() {
-        return _ptr;
-    }
-private:
-    T*_ptr;
-};
+namespace test {
 
-int main()
-{
-    SmartPtr<int> sp1(new int);
-    SmartPtr<int> sp2(sp1);
+    template<class T>
+    class auto_ptr {
+    public:
+        auto_ptr(T* ptr) :_ptr(ptr)
+        {}
+
+        auto_ptr(auto_ptr& ap) {
+            _ptr = ap._ptr;
+            ap._ptr = nullptr;
+        }
+
+        ~auto_ptr() {
+            delete _ptr;
+            std::cout << "delete ptr" << "\n";
+        }
+        T& operator*() {
+            return *_ptr;
+        }
+        T* operator->() {
+            return _ptr;
+        }
+    private:
+        T* _ptr;
+    };
+}
+
+int main() {
+    test::auto_ptr<int> ap1(new int);
+    test::auto_ptr<int> ap2(ap1);
+
+    *ap1 = 1;
+    *ap2 = 1;
 
     return 0;
 }
+
+//template<class T>
+//class SmartPtr {
+//public:
+//    SmartPtr(T* ptr) :_ptr(ptr)
+//    {}
+//
+//    ~SmartPtr() {
+//        delete _ptr;
+//        std::cout << "delete ptr" << "\n";
+//    }
+//    T& operator*() {
+//        return *_ptr;
+//    }
+//    T* operator->() {
+//        return _ptr;
+//    }
+//private:
+//    T* _ptr;
+//};
+
+//int main()
+//{
+//    SmartPtr<int> sp1(new int);
+//    SmartPtr<int> sp2(sp1);
+//
+//    return 0;
+//}
+
+
+
 
 //int div()
 //{
