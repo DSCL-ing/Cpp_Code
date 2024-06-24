1d1acb88e809cdfadf7b52e2b35bdf4a35330088
chj <2639075023@qq.com>
2024-06-25 00:05:02 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,11 +1,4 @@
 ﻿#include<iostream>
-#include<list>
 
-//enum { yellow, orange };
-enum struct Color { red, yellow = 2, blue };
-int main() {
-    int a  = (int)Color::red;
-    printf("%d\n",a);
-    std::cout<< static_cast<int>(Color::yellow) <<std::endl;
-    return 0;
-}
+#include<chrono>
+
