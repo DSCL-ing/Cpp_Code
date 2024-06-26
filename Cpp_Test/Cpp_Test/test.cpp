b74c1d52ebe0f172dbcb05bb30290a177690cf38
chj <2639075023@qq.com>
2024-06-26 21:54:51 +0800

    模板 update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -2,20 +2,22 @@
 #include<thread>
 #include<chrono>
 
+template<class T>
+void func(std::chrono::duration<T> d) {
 
-
+}
 
 int main() {
+    func(std::chrono::duration<long long>());
+    func(std::chrono::seconds());
+
     std::cout<<std::this_thread::get_id()<<"\n";
 
     std::cout<<"sleep_for:10s"<<"\n";
-        auto start = std::chrono::high_resolution_clock::now();
-    std::this_thread::sleep_for<int>(std::chrono::duration<int>(10));
+    //std::this_thread::sleep_until
     std::cout<<"sleep_for:end"<<"\n";
-        auto end = std::chrono::high_resolution_clock::now();
-        auto ret = end - start;
-        std::cout<<"计时器计时时长:"<<ret.count()<<"纳秒 == "<<ret.count()/ 1000000000<<"秒" << "\n";
-        
+
+
     //std::chrono::nanoseconds ret = end - start;
     
 
