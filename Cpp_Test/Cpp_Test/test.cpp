ef454e2a22b1ce285ebbff7b369e6d9e6aafedd4
chj <2639075023@qq.com>
2024-06-27 22:26:02 +0800

    <thread>

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -2,6 +2,7 @@
 #include<thread>
 #include<chrono>
 #include<cstdlib>
+#include<functional>
 
 #pragma warning(disable:4996)
 
@@ -16,18 +17,24 @@ void func1() {
 
 void func2() {
     do {
-        std::cout << std::this_thread::get_id() << "\n";
+        //std::cout << std::this_thread::get_id() << "\n";
         _sleep(100);
     } while (true);
 }
 
+class A {
+public:
+    static void fun1(){}
+     void fun2(){std::cout<<"func2()"<<"\n"; }
+
+};
+
 int main() {
-    
-    std::thread t2(func2);
-    t2.detach();
-    _sleep(1000);
 
-
+    std::thread t1(std::bind(&A::fun2,A()));
+    t1.join();
+ 
+ 
     //std::thread t1(func1);
     //std::thread t2(func2);
     //t1.join();
