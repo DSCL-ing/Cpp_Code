16688173f88d44dfed5ba8a45cec48cbe14e4710
chj <2639075023@qq.com>
2024-06-27 20:23:07 +0800

    <thread>

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -5,28 +5,35 @@
 
 #pragma warning(disable:4996)
 
+//int g_val;
 
 void func1() {
     do {
         std::cout << std::this_thread::get_id() << "\n";
-    } while (true);
+    } while (1);
 }
 
 
 void func2() {
     do {
-        std::this_thread::yield();
         std::cout << std::this_thread::get_id() << "\n";
+        _sleep(100);
     } while (true);
 }
 
 int main() {
-
-    std::thread t1(func1);
+    
     std::thread t2(func2);
-    std::cout<<t1.get_id()<<"\n";
-    t1.join();
-    t2.join();
+    t2.detach();
+    _sleep(1000);
+
+
+    //std::thread t1(func1);
+    //std::thread t2(func2);
+    //t1.join();
+    //t2.detach();
+    //t2.join();
+    //_sleep(1000);
     return 0;
 }
 
