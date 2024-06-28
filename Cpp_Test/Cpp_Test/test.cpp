537a665ca9432c52252092d89ff00ad4ad3dcf38
chj <2639075023@qq.com>
2024-06-28 17:45:42 +0800

    <thread>

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -18,23 +18,21 @@ void func1() {
 void func2() {
     do {
         //std::cout << std::this_thread::get_id() << "\n";
-        _sleep(100);
-    } while (true);
+        //_sleep(100);
+    } while (0);
 }
 
-class A {
-public:
-    static void fun1(){}
-     void fun2(){std::cout<<"func2()"<<"\n"; }
-
-};
 
 int main() {
 
-    std::thread t1(std::bind(&A::fun2,A()));
-    t1.join();
- 
- 
+    //std::thread t1;
+    //if (t1.joinable() == true)
+    //{
+    //    t1.join();
+    //}
+
+    std::cout<<std::thread::hardware_concurrency()<<"\n";
+
     //std::thread t1(func1);
     //std::thread t2(func2);
     //t1.join();
