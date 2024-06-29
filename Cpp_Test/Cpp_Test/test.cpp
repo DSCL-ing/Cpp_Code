1321d34304796ec5c52f6741a5af08db410f49b8
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-06-29 11:01:49 +0800

    <mutex> -- call_once()

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -10,28 +10,29 @@ std::once_flag g_flag;
 class Singleton {
 public:
     Singleton(const Singleton& s) = delete;
-    Singleton& operator=(const Singleton&s) = delete;
+    Singleton& operator=(const Singleton& s) = delete;
     static Singleton* GetInstance() {
-        std::call_once(g_flag,[](){ std::cout<<"do once:"<<std::this_thread::get_id()<<"\n"; _instance = new Singleton; });
-        std::cout<<std::this_thread::get_id()<<"\n";
+        std::call_once(g_flag, []() {
+            std::this_thread::sleep_for(std::chrono::seconds(1));          //延迟1s
+            std::cout << "do once:" << std::this_thread::get_id() << "\n";
+            _instance = new Singleton; });
+        std::cout << std::this_thread::get_id() << "\n";
+        std::cout << _instance << "\n";
         return _instance;
-    }
-    static void func() {
-
     }
 
 private:
-    Singleton(){};
+    Singleton() {};
     static Singleton* _instance;
 };
 Singleton* Singleton::_instance = nullptr;
 
 
 int main() {
-   std::thread t1(Singleton::GetInstance);
-   std::thread t2(Singleton::GetInstance);
-   std::thread t3(Singleton::GetInstance);
-    
+    std::thread t1(Singleton::GetInstance);
+    std::thread t2(Singleton::GetInstance);
+    std::thread t3(Singleton::GetInstance);
+
     t1.join();
     t2.join();
     t3.join();
