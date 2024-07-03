3e07c51967ccbb0f7fef77132498b8c52c54bf59
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-03 22:39:57 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -3,45 +3,42 @@
 #include<chrono>
 #include<cstdlib>
 #include<mutex>
+#include<queue>
 
-#pragma warning(disable:4996)
+template<class T>
+class  TaskQueue{
 
-int g_val;
-std::mutex mtx;
-
-void other() {
-    std::cout << "do other thing" << "\n";
-}
-
-void func() {
-    while (true) {
-        _sleep(100);
-        //while (mtx.try_lock() == false)
-        //    other();
-        //std::cout << g_val++ << "\n";
-        mtx.unlock();
+    //Task Type == T
+    //定长队列+自动管理增删
+    //同步 == 独占互斥锁
+    //生产者生产 == 增加 -- bool?生产成功返回true,放不下false --- (生产者绝对知道自己已生产)生产:通知消费者
+    //消费者消费 == 删除 -- bool?消费成功返回true,没有了false --- (同理)                    消费:通知生产者
+public:
+    bool isEmpty() {
+        return _bqueue.empty();
     }
-}
+    bool isFull() {
+        return _bqueue.size() == _capacity;
+    }
+    //AddTask
+    bool Push(const T&t) {
+        std::unique_lock<std::mutex> ul(_mtx);
+        std::lock_guard
+    }
+    //DelTask
+    bool Pop() {
 
+    }
+
+
+private:
+    std::queue<T> _bqueue;
+    std::mutex _mtx;
+    size_t _capacity; //队列定长大小
+};
 
 
 int main() {
-    std::recursive_timed_mutex t;
-    std::thread t1(func);
-    std::thread t2(func);
-    std::thread t3(func);
-
-    t1.join();
-    t2.join();
-    t3.join();
-
-
-    //std::thread t1(func1);
-    //std::thread t2(func2);
-    //t1.join();
-    //t2.detach();
-    //t2.join();
-    //_sleep(1000);
     return 0;
 }
 
---
+++ Cpp_Test/Cpp_Test/源.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <thread>
+#include <windows.h>
+
+using namespace std;
+
+void mouse_keep_click_right() //右键连点
+{
+    cout << "UP启动当前位置鼠标连击，DOWN关闭鼠标连击；Home回到菜单。" << endl;
+    POINT p;
+    HWND hdc;
+    while (1) {
+        if (GetAsyncKeyState(VK_UP) & 0x8000) {
+            cout << "鼠标点击开始！" << endl;
+            GetCursorPos(&p);
+            hdc = WindowFromPoint(p);
+            ScreenToClient(hdc, &p);
+            while (1) {
+                PostMessage(hdc, WM_RBUTTONDOWN, MK_RBUTTON, MAKELONG(p.x, p.y));
+                Sleep(10);
+                PostMessage(hdc, WM_RBUTTONUP, 0, MAKELONG(p.x, p.y));
+                if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+                    cout << "鼠标点击结束！" << endl;
+                    break;
+                }
+            }
+        }
+        if (GetAsyncKeyState(VK_HOME) & 0x8000) {
+            system("cls");
+            cout << "选择模式:←方向键为左键连点模式,→方向键为右键连点模式" << endl;
+            break;
+        }
+        Sleep(20);
+    }
+}
+
+void mouse_keep_click() //左键连点
+{
+    cout << "UP启动当前位置鼠标连击，DOWN关闭鼠标连击；Home回到菜单。" << endl;
+    POINT p;
+    HWND hdc;
+    while (1) {
+        if (GetAsyncKeyState(VK_UP) & 0x8000) {
+            cout << "鼠标点击开始！" << endl;
+            GetCursorPos(&p);
+            hdc = WindowFromPoint(p);
+            ScreenToClient(hdc, &p);
+            while (1) {
+                PostMessage(hdc, WM_LBUTTONDOWN, MK_LBUTTON, MAKELONG(p.x, p.y));
+                Sleep(10);
+                PostMessage(hdc, WM_LBUTTONUP, 0, MAKELONG(p.x, p.y));
+                if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+                    cout << "鼠标点击结束！" << endl;
+                    break;
+                }
+            }
+        }
+        if (GetAsyncKeyState(VK_HOME) & 0x8000) {
+            system("cls");
+            cout << "选择模式:←方向键为左键连点模式,→方向键为右键连点模式" << endl;
+            break;
+        }
+        Sleep(20);
+    }
+}
+
+int main()
+{
+
+#if 0
+    cout << "鼠标连击程序开始启动...." << endl;
+    cout << "UP启动当前位置鼠标连击，DOWN关闭鼠标连击；Home回到菜单。" << endl;
+    thread t1(mouse_keep_click);
+    t1.join();
+#endif
+
+#if 1
+    cout << "鼠标连击程序开始启动...." << endl;
+    cout << "选择模式:←方向键为左键连点模式,→方向键为右键连点模式" << endl;
+    while (1)
+    {
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+        {
+            std::cout << "左键连点模式" << std::endl;
+            thread t1(mouse_keep_click);
+            t1.join();
+        }
+        else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+        {
+            std::cout << "右键连点模式" << std::endl;
+            thread t2(mouse_keep_click_right);
+            t2.join();
+        }
+        else if (GetAsyncKeyState(VK_END) & 0x8000) {
+            cout << "程序结束！" << endl;
+            break;
+        }
+        //Sleep(20);
+    }
+#endif
+
+    return 0;
+}
+
