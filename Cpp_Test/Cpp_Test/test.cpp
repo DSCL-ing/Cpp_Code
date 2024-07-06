4d90a17bd7bd019188e484812e66dd650f545027
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-06 18:01:07 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,29 +1,23 @@
-﻿#include<iostream> //std::cout 
-#include<thread> //std::thread
-#include<queue> //std::queue
-#include<condition_variable> //std::condition_variable
-#include<mutex> //std::mutex, std::unique_lock, std::lock_guard
-#include<functional> //std::bind
+﻿#include<iostream>                  //std::cout 
+#include<thread>                    //std::thread
+#include<queue>                     //std::queue
+#include<condition_variable>        //std::condition_variable
+#include<mutex>                     //std::mutex, std::unique_lock, std::lock_guard
+#include<functional>                //std::bind
 #include <cstring>
 #include<atomic>
 
-
-class Text {
-public:
-    void bad(const std::string& parm)const;
-private:
-    char* _text;
-};
-void Text::bad(const std::string& parm) const
-{
-    for (int ix = 0; ix < parm.size(); ++ix)
-    {
-        _text[ix] = parm[ix];  // 不好的风格，但不是错误     
+template<class T>
+struct A {
+    operator T() {
+        return ++_a;
     }
-    //_text = parm.c_str();  // 错误：不能修改数据成员
-}
-
+    T _a = 0;
+};
 
 int main() {
-    std::atomic<int> a;
+    A<int> a;
+    std::cout << a << "\n";
+    //std::cout<<
+    std::cin << 1<<"\n";
 }
