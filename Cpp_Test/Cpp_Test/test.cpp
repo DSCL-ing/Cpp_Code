32a3e62b0de4a98f66dd29ce6646aa998df4c3fe
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-07 22:50:18 +0800

    <future>

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -7,12 +7,41 @@
 #include <atomic>
 #include <functional>
 #include<chrono>
+#include<future>
 
 
-int main()
-{
-    std::atomic_int a_i = 10;
-    std::cout<<a_i<<"\n";
+//#include <iostream>
+//#include <thread>
+
+//int main() {
+//    int x = 5;
+//    int y = 7;
+//
+//    // 创建一个线程，执行一个lambda表达式，捕获x和y的引用
+//    std::thread t([&]() {
+//        std::cout << "Sum: " << x++ + ++y << std::endl;
+//        });
+//
+//    // 等待线程完成
+//    t.join();
+//
+//    return 0;
+//}
+
+
+int main() {
+    std::promise<std::string>  pro;
+    std::thread t([&](){
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        pro.set_value("hello future");
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    });
+    
+    std::future<std::string> f = pro.get_future();
+    f.wait();
+    std::cout<<"get value:"<<f.get();
+
+   t.join();
+
     return 0;
 }
-
