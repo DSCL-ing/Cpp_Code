98973bef3a77c3f5b14bd4b3dad776ff4b691070
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-11 21:03:14 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -8,6 +8,45 @@
 #include <functional>
 #include<chrono>
 #include<future>
+#include<set>
+#include<string>
+
+void solution(std::string& str) {
+    bool flag = false;
+    int start = 0;
+    int end = str.size() - 1;
+    for (start; start <= end; start++) {
+        for (int i = start; i <= end;) {
+            if (i != end && str[i] == str[i + 1]) {
+                i = i + 1;
+            }
+            else {
+                if (start<i&&str[start] == str[i]) {
+                    str.erase(start ,i-start+1);
+                    end = str.size() - 1;
+                    flag = true;
+                }
+                break;
+            }
+        }
+        if (flag == true) {
+            break;
+        }
+    }
+    if (flag == false) {
+        return;
+    }
+    solution(str);
+}
+
+int main() {
+    std::string str;
+    while (std::cin >> str) {
+        solution(str);
+        std::cout << str << "\n";
+    }
+}
+
 
 
 //#include <iostream>
@@ -29,19 +68,19 @@
 //}
 
 
-int main() {
-    std::promise<std::string>  pro;
-    std::thread t([&](){
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        pro.set_value("hello future");
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    });
-    
-    std::future<std::string> f = pro.get_future();
-    f.wait();
-    std::cout<<"get value:"<<f.get();
-
-   t.join();
-
-    return 0;
-}
+//int main() {
+//    std::promise<std::string>  pro;
+//    std::thread t([&](){
+//        std::this_thread::sleep_for(std::chrono::seconds(2));
+//        pro.set_value("hello future");
+//        std::this_thread::sleep_for(std::chrono::seconds(2));
+//    });
+//    
+//    std::future<std::string> f = pro.get_future();
+//    f.wait();
+//    std::cout<<"get value:"<<f.get();
+//
+//   t.join();
+//
+//    return 0;
+//}
