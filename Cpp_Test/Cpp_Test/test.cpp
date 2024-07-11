98683112faf7a7ddadf0d85512f1050c5e1a819a
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-11 22:50:20 +0800

    <async>

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -1,71 +1,50 @@
-﻿#include<iostream>                  //std::cout 
-#include<thread>                    //std::thread
-#include<queue>                     //std::queue
-#include <iostream>
+﻿#include <iostream>
 #include <thread>
-#include <mutex>
-#include <atomic>
 #include <functional>
 #include<chrono>
 #include<future>
-#include<set>
-#include<string>
 
-void solution(std::string& str) {
-    bool flag = false;
-    int start = 0;
-    int end = str.size() - 1;
-    for (start; start <= end; start++) {
-        for (int i = start; i <= end;) {
-            if (i != end && str[i] == str[i + 1]) {
-                i = i + 1;
-            }
-            else {
-                if (start<i&&str[start] == str[i]) {
-                    str.erase(start ,i-start+1);
-                    end = str.size() - 1;
-                    flag = true;
-                }
-                break;
-            }
+void func(std::promise<std::string>& pro) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    pro.set_value("hello world");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+}
+
+int main() {
+
+    std::promise<std::string> pro;
+    std::thread t1(func, std::ref(pro));
+
+    auto fut = pro.get_future();
+    while (true) {
+        bool flag = false;
+        std::future_status ret = fut.wait_for(std::chrono::seconds(1));
+        switch (ret) {
+        case std::future_status::deferred:              //只有async函数才有效
+            std::cout << "异步线程还未启动" << "\n";
+            break;
+        case std::future_status::ready:
+            std::cout << "结果已就绪" << "\n";
+            flag = true;
+            break;
+        case std::future_status::timeout:
+            std::cout << "等待超时" << "\n";
+            break;
+        default:
+            std::cout << "结果异常" << "\n";
         }
         if (flag == true) {
             break;
         }
+        std::cout << "do something..." << "\n";
+        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
     }
-    if (flag == false) {
-        return;
-    }
-    solution(str);
+    std::cout << "输出结果::" << fut.get() << "\n";
+
+    t1.join();
+    return 0;
 }
-
-int main() {
-    std::string str;
-    while (std::cin >> str) {
-        solution(str);
-        std::cout << str << "\n";
-    }
-}
-
-
-
-//#include <iostream>
-//#include <thread>
-
-//int main() {
-//    int x = 5;
-//    int y = 7;
-//
-//    // 创建一个线程，执行一个lambda表达式，捕获x和y的引用
-//    std::thread t([&]() {
-//        std::cout << "Sum: " << x++ + ++y << std::endl;
-//        });
-//
-//    // 等待线程完成
-//    t.join();
-//
-//    return 0;
-//}
 
 
 //int main() {
