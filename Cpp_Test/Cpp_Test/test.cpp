a28fcdc7f3f8198af5e0371f7759256ad1cd0e55
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-07-14 22:48:57 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -5,42 +5,12 @@
 #include<queue>
 #include<mutex>
 
-class BlockQueue {
-public:
-    BlockQueue(const BlockQueue& q) = delete;
-    BlockQueue& operator=(const BlockQueue& q) = delete;
-
-    void Push() {
-
-    }
-
-    void Pop() {
-
-    }
-
-    static BlockQueue& GetInstance() {
-        if (_instance.load() == nullptr) {
-            std::lock_guard<std::mutex>lg(_mtx);
-            if (_instance.load() == nullptr) {
-                _instance.store(new BlockQueue); //内存顺序不会打乱,安全
-            }
-        }
-        return *(_instance.load());
-    }
-
-private:
-    BlockQueue() {}
-    std::queue<int> _q;
-    int _capacity = 5;
-    static std::mutex _mtx;
-    static std::atomic<BlockQueue*> _instance;
-};
-std::atomic<BlockQueue*> BlockQueue::_instance = nullptr;
-std::mutex BlockQueue::_mtx;
+//子弹类 
+//金子弹
+//银子弹
+//铜子弹
 
 
 int main() {
-    BlockQueue::GetInstance();
-
     return 0;
 }
