55069b4cae0dbe5a8491c1ef6f7f389ec2c46ef8
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-08-08 22:50:43 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -4,6 +4,26 @@
 #include<atomic>
 #include<queue>
 
-int main() {
-    return 0;
+#include<cassert>
+typedef int HeapDataType;
+
+void Swap(HeapDataType* p1, HeapDataType* p2)
+{
+	HeapDataType tmp = 0;
+	tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+//向上调整算法：从0插入建堆 以及 在堆的前提下插入 //不适合直接建堆（把数组从1开始向下建堆）
+void AdjustUp(HeapDataType* a, int child)
+{
+	assert(a);
+	int parent = (child - 1) / 2;
+	while (parent >= 0 && child > 0 && a[child] > a[parent]) //小堆:a[child] < a[parent]
+	{														  
+		Swap(&a[child], &a[parent]);
+		child = parent;
+		parent = (child - 1) / 2;
+	}
 }
