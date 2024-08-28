7377a33a283e25451774d46649e00a694e909b84
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-08-28 21:58:56 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -5,3 +5,30 @@
 #include<queue>
 #include<stack>
 
+#include"25.2 RBTree.hpp"
+
+template<class K>
+class my_set {
+    bool Insert(const K& key) {
+        return t.Insert(std::make_pair(key,key));
+    }
+
+    //Node* Find(const K& key) {
+    //    return  t.Find(key);
+    //}
+    
+    RBTree<K,K> t;
+};
+
+template<class K, class V>
+class my_map {
+    bool Insert(const std::pair<K, V>& kv) {
+        return t.Insert(kv);
+    }
+    RBTree<K,V> t;
+};
+
+int main() {
+    my_set<int> s;
+    my_map<int,int> m;
+}
