1ca62b90bc3404d1f1715bae4a1174ae91f4f2d3
DSCLing <102305029+DSCL-ing@users.noreply.github.com>
2024-10-09 01:58:16 +0800

    update

--- Cpp_Test/Cpp_Test/test.cpp
+++ Cpp_Test/Cpp_Test/test.cpp
@@ -5,111 +5,150 @@
 #include<atomic>
 #include<queue>
 #include<stack>
+#include<memory>
 
 #if 1
-const int N = 20;       //要跑20圈
-int size = 0;           //方案数
+// bugTest.cpp: 定义应用程序的入口点。
+//
 
-void Print(const std::vector<int>& v) {
-    for (int i = 1 ; i<v.size();i++) {
-        if (v[i] == 1) std::cout << i << " " ;
-    }
-    std::cout<<std::endl;
-}
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 
-//dfs(下一次待跑圈数,已跑圈数)
-void dfs(int cnt, int ans, std::vector<int>& v) {
-    if (ans + cnt  > 20)     //
+class BugTemplate {
+public:
+    virtual void FixTest() = 0;
+};
+
+//存在1个bug
+class ClassBug :public BugTemplate
+{
+private:
+    const std::string StringToUpper(const std::string& src)
     {
-        return ;
+        std::string temp;
+        temp = src;
+        //std::transform(输入起始,输入结束,输出起始,处理函数);
+        std::transform(temp.begin(), temp.end(), temp.begin(), ::toupper);
+        return temp;    //返回拷贝
     }
-    if (ans + cnt == 20) 
+
+    const std::string StringToLower(const char* src)
     {
-        v[cnt] = 1;
-        Print(v);
-        v[cnt] = 0; //回溯
-        size++;
-        return ;
+        std::string temp;
+        temp = src;
+        std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        return temp;
     }
 
-    v[cnt] = 1; //可以跑
-    for (int i = cnt+1; i < N-ans; i++)      
-    /*
-     i<N-ans,规律
-     (more) (next)
-      1  --  19    1,2 -- 17      1,2,3 -- 14  即 ans(已跑圈数) <--> N-ans(要跑圈数)
-      2  --  18 
-      3  --  17
-      ...
-    */
+public:
+    ClassBug() {}
+    ~ClassBug() {}
+    //需要修复乱码问题
+    //tip:考察对类的作用域理解
+    virtual void FixTest()  override//不上override,提高可读性
     {
-        dfs(i, ans+cnt, v); //下一次执行(待跑圈数,已跑圈数);
-    }
-    v[cnt] = 0; //回溯
-}
+        std::cout << "请修复bug，使程序正确输出转大写转小写" << std::endl;
+        const char* test = "ABCddasdasdaddaweqWADfFcddasdasdaddaweqewabcddasdasdaddaweWQD";
+        std::string test1 = StringToUpper(test);
+        //const char* test2 = StringToLower(test).c_str();  //该函数返回的是一个临时对象,char*又指向的是一个由临时对象管理的资源,临时对象一销毁,char*就是垂悬指针了;因此不能使用指针,需要使用对象来拷贝或移动使资源的生命周期延长;
+        std::string test2 = StringToLower(test).c_str();    //不需要显式使用std::move,现在编译器会自动优化,比使用效果更好
 
-int main() {
-    std::vector<int> v;
-    v.resize(21, 0);
+        std::cout << test << std::endl;
+        std::cout << test1 << std::endl;
+        std::cout << test2 << std::endl;
 
-    //第一步的圈数,范围1到20圈
-    for (int i = 1; i <= N; i++) {
-        dfs(i, 0, v);
     }
-    std::cout<<"总方案数: "<<size << std::endl;
+
+};
+
+
+struct buffertest {
+    char strbuffer1[9];
+    char strbuffer2[9];
+    char strbuffer3[9];
+};
+
+//存在3个bug
+class CopyBug :public BugTemplate
+{
+public:
+    CopyBug() {}
+    ~CopyBug() {}
+    //需要修复乱码问题
+    virtual void FixTest()
+    {
+
+        std::cout << "请让程序能够正常输出ABCDEFGHIJKLMNOP" << std::endl;
+        buffertest* buffer = (buffertest*)malloc(sizeof(buffertest));   //bug1:内存泄漏 -> 智能指针或free
+        //bug2:malloc可能不会成功返回有效空间指针,例如内存不足时可能会返回NULL,因此需要检测
+        if (buffer == nullptr) {
+            perror("malloc is NULL");
+            return ;
+            //exit;
+            //assert(false);
+            //...
+        }
+
+        //bug3:malloc的对象是未初始化的,可能存在不确定的值 -> 使用memset清零
+        memset(buffer,0,sizeof(buffer));
+        
+        //bug4:字符串常量隐藏了一个\0,大小是字面值的个数+1;因此解决方法可以是给buffer的数组扩大容量
+        strcpy(buffer->strbuffer2, "IJKLMNOP");
+        strcpy(buffer->strbuffer1, "ABCDEFGH");
+        //也可以使用strncpy,strncpy,保证buffer不会溢出;
+
+        std::cout << buffer->strbuffer1;
+        std::cout << buffer->strbuffer2 << std::endl;
+        
+        free(buffer);
+    }
+};
+
+//存在一个bug
+class VectorBug :public BugTemplate
+{
+public:
+    VectorBug() {}
+    ~VectorBug() {}
+    virtual void FixTest()
+    {
+        std::vector<int> vec = { 1,2,3,4,5,6,7,8,9 };
+        std::cout << "正确使用迭代器删除程序前三个数值,使程序不崩溃" << std::endl;
+        std::vector<int>::iterator it = vec.begin();
+        for (int i = 0; i < 3; i++)
+        {
+            it = vec.erase(it); //erase后会返回删除前该元素的下一个元素位置的迭代器
+            //it++; //迭代器失效了,不能再++
+        }
+        for (it = vec.begin(); it != vec.end(); it++)
+        {
+            std::cout << *it << " ";
+        }
+
+    }
+};
+
+
+//修复bug 使程序正常运作
+int main()
+{
+    {
+        ClassBug bug;
+        bug.FixTest();
+    }
+    {
+        CopyBug bug;
+        bug.FixTest();
+    }
+    {
+        VectorBug bug;
+        bug.FixTest();
+    }
+    system("pause");
     return 0;
 }
-
-//using namespace std;
-//
-//// 用于存储所有方案
-//vector<vector<int>> vv;
-//
-//// 暴力递归生成所有分配方案
-//void solution(int n, vector<int>& v) {
-//    if (n == 0) {
-//        // 如果圈数已经跑完，则检查是否满足递增条件
-//        bool flag = true;
-//        for (int i = 1; i < v.size(); ++i) {
-//            if (v[i] <= v[i - 1]) {
-//                flag = false;
-//                break;
-//            }
-//        }
-//        // 如果满足条件，保存该方案
-//        if (flag) {
-//            vv.push_back(v);
-//        }
-//        return;
-//    }
-//
-//    // 遍历剩余圈数，生成所有可能的圈数组合
-//    for (int i = 1; i <= n; ++i) {
-//        v.push_back(i);  // 添加当前跑的圈数
-//        solution(n - i, v);  // 递归处理剩余的圈数
-//        v.pop_back();  // 移除当前圈数
-//    }
-//}
-//
-//int main() {
-//    const int N = 20;  // 总圈数
-//    vector<int> v;
-//
-//    // 生成所有跑完 20 圈的暴力方案
-//    solution(N, v);
-//
-//    // 输出总方案数
-//    cout << "总方案数: " << vv.size() << endl;
-//
-//    // 输出每种方案
-//    for (const auto& e : vv) {
-//        for (size_t i = 0; i < e.size(); ++i) {
-//            cout << e[i] << (i == e.size() - 1 ? "" : ",");
-//        }
-//        cout << endl;
-//    }
-//    return 0;
-//}
 
 
 
